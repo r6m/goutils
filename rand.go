b8e7848c6edce19a860ba8e3c78bcb5fd4052f01
Rezam <light.reza@gmail.com>
2019-10-28 09:21:41 +0330

    feat: add RandStringBytes for advcanced random strings

--- rand.go
+++ rand.go
@@ -29,3 +29,38 @@ func RandString(n int) string {
 	}
 	return hex.EncodeToString(bytes)
 }
+
+const digitsBytes = "0123456789"
+const lowerBytes = "abcdefghijklmnopqrstuvwxyz"
+const upperBytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+type RandMode int
+
+const (
+	Digits = 1 << iota
+	Lower
+	Upper
+	Humanize
+)
+
+// RandStringBytes returns a random string based on given flag.
+// multiple flags can be combined: Digits|Lower
+func RandStringBytes(n int, mode RandMode) string {
+	source := make([]byte, 0)
+	if mode&(Digits) != 0 {
+		source = append(source, []byte(digitsBytes)...)
+	}
+	if mode&(Lower) != 0 {
+		source = append(source, []byte(lowerBytes)...)
+	}
+	if mode&(Upper) != 0 {
+		source = append(source, []byte(upperBytes)...)
+	}
+
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = source[rand.Intn(len(source))]
+	}
+
+	return string(b)
+}
